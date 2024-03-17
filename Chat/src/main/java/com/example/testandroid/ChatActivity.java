71954bdc81072a7352f2ff2224eceb701a2164bd
Kiến Huy Lý <kienhuy1411@gmail.com>
2024-03-17 13:38:35 +0700

    New UI 17/3/2024

--- Chat/src/main/java/com/example/testandroid/ChatActivity.java
+++ Chat/src/main/java/com/example/testandroid/ChatActivity.java
@@ -6,18 +6,46 @@ import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.testandroid.databinding.ActivityMainBinding;
 
 public class ChatActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.chats);
-        LinearLayout menu= findViewById(R.id.menu_layout);
-        ConstraintLayout chats= findViewById(R.id.chats_layout);
-        if (menu != null) {
-           chats.addView(menu);
-        } else {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new ChatFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
-        }
+            switch (item.getItemId()){
+
+                case R.id.ChatList:
+                    replaceFragment(new ChatFragment());
+                    break;
+                case  R.id.CallList:
+                    replaceFragment(new CallFragment());
+                    break;
+                case R.id.Communication:
+                    replaceFragment(new CommunicationFragment());
+                    break;
+                case R.id.Setting:
+                    replaceFragment(new SettingFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
--- Chat/src/main/java/com/example/testandroid/SignUpActivity.java
+++ Chat/src/main/java/com/example/testandroid/SignUpActivity.java
@@ -11,7 +11,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signup);
         TextView signin= findViewById(R.id.linksignin);
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
