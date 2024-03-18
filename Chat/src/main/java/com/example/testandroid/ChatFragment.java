271d8620b80d4154516f100d19a990d8e41c9bf6
Kiến Huy Lý <kienhuy1411@gmail.com>
2024-03-18 07:47:16 +0700

    New UI 18/3

--- Chat/src/main/java/com/example/testandroid/ChatFragment.java
+++ Chat/src/main/java/com/example/testandroid/ChatFragment.java
@@ -1,5 +1,7 @@
 package com.example.testandroid;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +18,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ChatFragment extends Fragment {
-
+    private ListView listView;
+    Activity context;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,7 +63,15 @@ public class ChatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        context=getActivity();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+    public void onStart(){
+        super.onStart();
+        String[] items={"Admin"};
+        listView = (ListView) context.findViewById(R.id.ListChat);
+        ChatListAdapter adapter = new ChatListAdapter(items,context);
+        listView.setAdapter(adapter);
     }
 }
--- Chat/src/main/java/com/example/testandroid/CommunicationFragment.java
+++ Chat/src/main/java/com/example/testandroid/CommunicationFragment.java
@@ -1,5 +1,6 @@
 package com.example.testandroid;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +16,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CommunicationFragment extends Fragment {
-
+    private ListView listView;
+    Activity context;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -59,6 +62,14 @@ public class CommunicationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        context=getActivity();
         return inflater.inflate(R.layout.fragment_communication, container, false);
+    }
+    public void onStart(){
+        super.onStart();
+        String[] items={"Group"};
+        listView = (ListView) context.findViewById(R.id.ListCommunication);
+        ChatListAdapter adapter = new ChatListAdapter(items,context);
+        listView.setAdapter(adapter);
     }
 }
